27d1244aa2e311a408e1399281368f6796df4bff
Bhaa Shakur <bhaa.shakur@gmail.com>
2019-10-22 09:59:48 +0100

    Add feature to filter recipes

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -257,3 +257,55 @@ class RecipeImageUploadTests(TestCase):
         res = self.client.post(url, {'image': 'notimage'}, format='multipart')
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_filter_recipes_by_tagss(self):
+        """Test returning recipies with specific tags"""
+        recipe1 = sample_recipe(user=self.user, title="Thai vegetable curry")
+        recipe2 = sample_recipe(user=self.user, title="Aubergine with tahini")
+
+        tag1 = sample_tag(user=self.user, name="Vegan")
+        tag2 = sample_tag(user=self.user, name="Vegetarian")
+
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+
+        recipe3 = sample_recipe(user=self.user, title='Fish and chips')
+
+        res = self.client.get(
+            RECIPES_URL,
+            {'tags': ",".join([str(t.id) for t in [tag1, tag2]])}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
+    def test_filter_recipies_by_ingredients(self):
+        """Tests returning recipies with specific ingredients"""
+        recipe1 = sample_recipe(user=self.user, title="Posh beans on toast")
+        recipe2 = sample_recipe(user=self.user, title="Chicken cacciatore")
+
+        ing1 = sample_ingredient(user=self.user, name="Feta cheese")
+        ing2 = sample_ingredient(user=self.user, name="Chicken")
+
+        recipe1.ingredients.add(ing1)
+        recipe2.ingredients.add(ing2)
+
+        recipe3 = sample_recipe(user=self.user, title='Steak')
+
+        res = self.client.get(
+            RECIPES_URL,
+            {'ingredients': ",".join([str(t.id) for t in [ing1, ing2]])}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
